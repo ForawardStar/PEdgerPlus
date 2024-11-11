136e4ec89bbedb669232d46a07da09b63495bc6d
ForawardStar <32625467+ForawardStar@users.noreply.github.com>
2024-11-11 16:57:24 +0800

    Add files via upload

--- test_edge.py
+++ test_edge.py
@@ -1,6 +1,5 @@
 import caffe
 import numpy as np
-import matplotlib.pyplot as plt
 from skimage import feature
 import cv2
 import os
@@ -9,27 +8,58 @@ import time
 def sigmoid(x):
     return np.exp(x - 0.5) / (np.exp(x - 0.5) + np.exp(0.5 - x))
 
-# 设置模式
+# Running Mode
 caffe.set_mode_cpu()
 
-# 加载模型
-net = caffe.Net('/home/fyb/caffe-tools-master/examples/bsds500/test.prototxt', '/home/fyb/caffe-tools-master/examples/bsds500/snapshot_iter_9500.caffemodel', caffe.TEST)
+# Loading Pre-trained Checkpoints
+net1 = caffe.Net('models/EdgeModel1.prototxt', 'models/EdgeModel.caffemodel', caffe.TEST)
 
-print("type:{}  ,  shape:{}".format(type(net.blobs['data'].data), net.blobs['data'].data.shape))
-# 加载图像
-save_path = "/home/fyb/PEdgerPlus/edges_caffetraining/"
+net2 = caffe.Net('models/EdgeModel2.prototxt', 'models/EdgeModel.caffemodel', caffe.TEST)
+
+
+print("type:{}  ,  shape:{}".format(type(net1.blobs['blob1'].data), net1.blobs['blob1'].data.shape))
+# Loading Images
+input_path = "data/HED-BSDS/test/"
+save_path = "edge_results/"
 os.makedirs(save_path, exist_ok=True)
+filenames = os.listdir(input_path)
+for filename in filenames:
+    image = caffe.io.load_image(input_path + filename)
+    if image.shape[0] == 481:
+        # Pre-processing images
+        transformer = caffe.io.Transformer({'data': net1.blobs['blob1'].data.shape})
+        transformer.set_transpose('data', (2,0,1)) # Converting HWC to CHW
+        net1.blobs['blob1'].data[...] = transformer.preprocess('data', image)
 
-for ii in range(200):
-    # 预处理图像
-    # 运行网络
-    net.forward()
+        # network inference
+        net1.forward()
 
-    # 获取特征图
-    features = net.blobs['conv4'].data.squeeze()
-    res = sigmoid(features)
-    print("res shape:{}  ,  features shape:{}".format(res.shape, features.shape))
+        # Generating results
+        features = net1.blobs['conv_blob119'].data.squeeze()
+        res = sigmoid(features)
+        print("image shape:{}  ,  features shape:{}".format(image.shape, features.shape))
 
-    # 显示结果
-    cv2.imwrite(save_path + str(ii) + ".png", res*255)
-    
+        # save results
+        cv2.imwrite(save_path + filename, res*255)
+    else:
+        # Pre-processing images
+        transformer = caffe.io.Transformer({'data': net2.blobs['blob1'].data.shape})
+        transformer.set_transpose('data', (2,0,1)) # Convert HWC to CHW
+        net2.blobs['blob1'].data[...] = transformer.preprocess('data', image)
+
+        # network inference
+        net2.forward()
+
+        # Generating results
+        features = net2.blobs['conv_blob119'].data.squeeze()
+        res = sigmoid(features)
+        print("image shape:{}  ,  features shape:{}".format(image.shape, features.shape))
+
+        # save results
+        cv2.imwrite(save_path + filename, res*255)
+
+
+
+
+
+
